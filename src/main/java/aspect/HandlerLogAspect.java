8062e47a453d3b90082f25febcaccde145b5b04b
mayuxing <rhythmdiao@163.com>
2015-03-20 16:03:03 +0800

    tiny change

--- src/main/java/aspect/HandlerLogAspect.java
+++ src/main/java/aspect/HandlerLogAspect.java
@@ -15,23 +15,18 @@ public final class HandlerLogAspect {
     private static final String EXECUTION = "execution(* handlers.*.execute(..))";
     private static final Logger logger = LoggerFactory.getLogger(HandlerLogAspect.class);
 
-    @Around(EXECUTION)
-    public JsonRestResult process(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    @Around(EXECUTION + "&&args(request)")
+    public JsonRestResult process(ProceedingJoinPoint proceedingJoinPoint, final Request request) throws Throwable {
         logger.info("Handler:{}", proceedingJoinPoint.getSignature().getDeclaringType().getSimpleName());
-        for (Object o : proceedingJoinPoint.getArgs()) {
-            if (o instanceof Request) {
-                Request request = (Request) o;
-                logger.info("request headers:");
-                Enumeration headerKeys = request.getHeaderNames();
-                while (headerKeys.hasMoreElements()) {
-                    Object key = headerKeys.nextElement();
-                    logger.debug("{}:{}", key, request.getHeader((String) key));
-                }
-                logger.info("request parameters:");
-                for (Object key : request.getParameterMap().keySet()) {
-                    logger.info("{}:{}", key, request.getParameter((String) key));
-                }
-            }
+        logger.info("request headers:");
+        Enumeration headerKeys = request.getHeaderNames();
+        while (headerKeys.hasMoreElements()) {
+            Object key = headerKeys.nextElement();
+            logger.debug("{}:{}", key, request.getHeader((String) key));
+        }
+        logger.info("request parameters:");
+        for (Object key : request.getParameterMap().keySet()) {
+            logger.info("{}:{}", key, request.getParameter((String) key));
         }
         JsonRestResult result = (JsonRestResult) proceedingJoinPoint.proceed();
         logger.info("response:{}", result.convertToResponse());
