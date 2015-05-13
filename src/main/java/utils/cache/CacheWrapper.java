539f104be4ea546a51c1a762e68945f51d27195d
mayuxing <rhythmdiao@163.com>
2015-05-13 16:06:29 +0800

    modify cache wrapper

--- src/main/java/utils/cache/CacheWrapper.java
+++ src/main/java/utils/cache/CacheWrapper.java
@@ -2,15 +2,15 @@ package utils.cache;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheBuilderSpec;
 import com.google.common.collect.Maps;
 
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.TimeUnit;
 
 public final class CacheWrapper {
     private static CacheWrapper instance = null;
 
-    private ConcurrentMap<String, Cache> cacheMap;
+    private ConcurrentMap<String, Cache<String, Object>> cacheMap;
 
     private CacheWrapper() {
         cacheMap = Maps.newConcurrentMap();
@@ -23,34 +23,26 @@ public final class CacheWrapper {
         return instance;
     }
 
-    public static synchronized Cache newCache(String cacheName) {
-        Cache cache = CacheBuilder.newBuilder().build();
+    public static synchronized Cache<String, Object> newCache(String cacheName) {
+        Cache<String, Object> cache = CacheBuilder.newBuilder().build();
         getInstance().cacheMap.putIfAbsent(cacheName, cache);
         return cache;
     }
 
-    public static synchronized Cache newCacheWithParameter(String cacheName, long maximumSize, long duration, TimeUnit timeUnit) {
-        Cache cache = CacheBuilder
-                .newBuilder()
-                .maximumSize(maximumSize)
-                .expireAfterWrite(duration, timeUnit)
-                .build();
+    public static synchronized Cache<String, Object> newCacheWithConfig(String cacheName, String config) {
+        Cache<String, Object> cache = CacheBuilder.from(CacheBuilderSpec.parse(config)).build();
         getInstance().cacheMap.putIfAbsent(cacheName, cache);
         return cache;
     }
 
-    public static synchronized Cache getCache(String cacheName) {
-        Cache cache = getInstance().cacheMap.getOrDefault(cacheName, CacheBuilder.newBuilder().build());
+    public static synchronized Cache<String, Object> getCache(String cacheName) {
+        Cache<String, Object> cache = getInstance().cacheMap.getOrDefault(cacheName, CacheBuilder.newBuilder().<String, Object>build());
         getInstance().cacheMap.putIfAbsent(cacheName, cache);
         return cache;
     }
 
-    public static synchronized Cache getCacheWithParameter(String cacheName, long maximumSize, long duration, TimeUnit timeUnit) {
-        Cache cache = getInstance().cacheMap.getOrDefault(cacheName, CacheBuilder
-                .newBuilder()
-                .maximumSize(maximumSize)
-                .expireAfterWrite(duration, timeUnit)
-                .build());
+    public static synchronized Cache<String, Object> getCacheWithConfig(String cacheName, String config) {
+        Cache<String, Object> cache = getInstance().cacheMap.getOrDefault(cacheName, CacheBuilder.from(CacheBuilderSpec.parse(config)).<String, Object>build());
         getInstance().cacheMap.putIfAbsent(cacheName, cache);
         return cache;
     }
