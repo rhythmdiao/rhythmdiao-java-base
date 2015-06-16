fe7678f197d3ad06cdd15b018c49fa751334a11a
mayuxing <rhythmdiao@163.com>
2015-06-16 17:44:48 +0800

    tiny change

--- general/src/main/java/com/rhythmdiao/utils/cache/CacheWrapper.java
+++ general/src/main/java/com/rhythmdiao/utils/cache/CacheWrapper.java
@@ -24,31 +24,31 @@ public final class CacheWrapper<K, V> {
         return instance;
     }
 
-    public static <K, V> Cache<K, V> newCache(String cacheName) {
+    public Cache<K, V> newCache(String cacheName) {
         Cache<K, V> cache = CacheBuilder.newBuilder().build();
         putIfAbsent(cacheName, cache);
         return cache;
     }
 
-    public static <K, V> Cache<K, V> newCache(String cacheName, String config) {
+    public Cache<K, V> newCache(String cacheName, String config) {
         Cache<K, V> cache = CacheBuilder.from(CacheBuilderSpec.parse(config)).build();
         putIfAbsent(cacheName, cache);
         return cache;
     }
 
-    public static <K, V> Cache<K, V> getOrDefault(String cacheName) {
+    public Cache<K, V> getOrDefault(String cacheName) {
         Cache<K, V> cache = (Cache<K, V>) getInstance().cacheMap.getOrDefault(cacheName, CacheBuilder.newBuilder().build());
         putIfAbsent(cacheName, cache);
         return cache;
     }
 
-    public static <K, V> Cache<K, V> getOrDefault(String cacheName, String config) {
+    public Cache<K, V> getOrDefault(String cacheName, String config) {
         Cache<K, V> cache = (Cache<K, V>) getInstance().cacheMap.getOrDefault(cacheName, CacheBuilder.from(CacheBuilderSpec.parse(config)).build());
         putIfAbsent(cacheName, cache);
         return cache;
     }
 
-    public static <K, V> void putIfAbsent(String cacheName, Cache<K, V> cache) {
+    private void putIfAbsent(String cacheName, Cache<K, V> cache) {
         getInstance().cacheMap.putIfAbsent(cacheName, cache);
     }
 }
