6a82cd9999642ed5ee4d17e6ad06d5876d964949
mayuxing <rhythmdiao@163.com>
2015-07-28 13:28:24 +0800

    tiny change

--- general/src/main/java/com/rhythmdiao/aspect/HandlerLogAspect.java
+++ general/src/main/java/com/rhythmdiao/aspect/HandlerLogAspect.java
@@ -1,18 +1,17 @@
 package com.rhythmdiao.aspect;
 
+import com.google.common.base.Strings;
 import com.rhythmdiao.annotation.RestfulHandler;
+import com.rhythmdiao.rest.result.BaseRestResult;
+import com.rhythmdiao.rest.result.json.JsonRestResult;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.eclipse.jetty.server.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.rhythmdiao.rest.result.BaseRestResult;
-import com.rhythmdiao.rest.result.json.JsonRestResult;
 
 import java.util.Enumeration;
-
-import static com.google.common.base.Strings.isNullOrEmpty;
 
 @Aspect
 public final class HandlerLogAspect {
@@ -21,24 +20,29 @@ public final class HandlerLogAspect {
 
     @Around(EXECUTION + "&&args(request)")
     public BaseRestResult processExecution(ProceedingJoinPoint proceedingJoinPoint, final Request request) throws Throwable {
-        LOG.debug("Handler:{}", proceedingJoinPoint.getSignature().getDeclaringType().getSimpleName());
-        LOG.debug("request headers:");
-        Enumeration headerKeys = request.getHeaderNames();
-        while (headerKeys.hasMoreElements()) {
-            Object key = headerKeys.nextElement();
-            LOG.debug("{}:{}", key, request.getHeader((String) key));
+        Class clazz = proceedingJoinPoint.getSignature().getDeclaringType();
+        
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Handler:{}", clazz.getSimpleName());
+            LOG.debug("request headers:");
+            Enumeration headerKeys = request.getHeaderNames();
+            while (headerKeys.hasMoreElements()) {
+                Object key = headerKeys.nextElement();
+                LOG.debug("{}:{}", key, request.getHeader((String) key));
+            }
+            LOG.debug("request parameters:");
+            for (Object key : request.getParameterMap().keySet()) {
+                LOG.debug("{}:{}", key, request.getParameter((String) key));
+            }
         }
-        LOG.debug("request parameters:");
-        for (Object key : request.getParameterMap().keySet()) {
-            LOG.debug("{}:{}", key, request.getParameter((String) key));
-        }
-        RestfulHandler restfulHandler = (RestfulHandler) proceedingJoinPoint.getSignature().getDeclaringType().getAnnotation(RestfulHandler.class);
+
+        RestfulHandler restfulHandler = (RestfulHandler) clazz.getAnnotation(RestfulHandler.class);
         final String identification = restfulHandler.identification();
         final String requestIdentification = request.getParameter("identification");
         BaseRestResult result;
-        if (isNullOrEmpty(identification)) {
+        if (Strings.isNullOrEmpty(identification)) {
             result = (BaseRestResult) proceedingJoinPoint.proceed();
-        } else if (!isNullOrEmpty(identification) && !isNullOrEmpty(requestIdentification) && identification.equals(requestIdentification)) {
+        } else if (!Strings.isNullOrEmpty(identification) && !Strings.isNullOrEmpty(requestIdentification) && identification.equals(requestIdentification)) {
             result = (BaseRestResult) proceedingJoinPoint.proceed();
         } else {
             result = new JsonRestResult();
---
+++ general/src/main/java/com/rhythmdiao/rest/result/Result.java
@@ -0,0 +1,5 @@
+package com.rhythmdiao.rest.result;
+
+public interface Result {
+    String convertResult();
+}
