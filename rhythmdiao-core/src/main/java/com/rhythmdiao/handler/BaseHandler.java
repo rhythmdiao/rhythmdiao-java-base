d3a5aabc23b12bf251eb3b1984d6604945c192ab
mayuxing <rhythmdiao@163.com>
2015-12-08 17:26:27 +0800

    tiny change & modify config files

--- rhythmdiao-core/src/main/java/com/rhythmdiao/handler/BaseHandler.java
+++ rhythmdiao-core/src/main/java/com/rhythmdiao/handler/BaseHandler.java
@@ -4,18 +4,11 @@ import com.rhythmdiao.entity.HandlerMetaData;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 public abstract class BaseHandler implements Handler {
     private HttpServletRequest request;
     private HttpServletResponse response;
     private HandlerMetaData handlerMetaData;
-
-    public void addResponseHeader(Map<String, String> headers) {
-        for (Map.Entry<String, String> header : headers.entrySet()) {
-            response.addHeader(header.getKey(), header.getValue());
-        }
-    }
 
     public HttpServletRequest getRequest() {
         return request;
